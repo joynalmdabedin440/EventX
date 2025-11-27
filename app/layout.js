01ca5c528244b3194cdbca57819c19893358f5b5
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-11-27 19:28:24 +0600

    add pink and orange glow effect

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 
@@ -10,28 +11,40 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-         className={`bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white`}
+        className={`bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white`}
       >
 
-        {/* Header */}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* Header */}
 
-        <main className=" relative min-h-screen container mx-auto pt-40 md:pt-32">
-          {/* glow */}
-          <div>
+          <main className=" relative min-h-screen container mx-auto pt-40 md:pt-32">
+            {/* glow */}
+            <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+              <div className="absolute top-0 left-1/4 h-96 w-96 bg-pink-600/20 rounded-full blur-3xl"/>
+              <div className="absolute bottom-0 right-1/4 h-96 w-96 bg-orange-600/20 rounded-full blur-3xl"/>
+              
 
-          </div>
+            </div>
 
-          <div>
-            {children}
+            <div className="relative z-10">
+              {children}
 
-          </div>
-          
-          
-          {/* Footer */}
+            </div>
 
-        </main>
+
+            {/* Footer */}
+
+          </main>
+        </ThemeProvider>
+
+
       </body>
     </html>
   );
