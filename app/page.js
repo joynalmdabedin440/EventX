2517633c331906cd58200944593b14ccafe117ef
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-11-29 20:01:44 +0600

    add button

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Button variant="destructive">my first Next.js app</Button>
+      <Button variant="destructive">Click on</Button>
       </div>
   );
 }
