0810b5cf5705a0fab51c6c9ce3afa036cb699f3a
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-12-03 00:57:37 +0600

    add clerk signin and signUp button

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -5,7 +6,7 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-            <nav className='fixed top-0 left-0 right-0 bg-black/70 backdrop-blur-xl z-20 border-b'>
+            <nav className='fixed top-0 left-0 right-0 bg-background/5 backdrop-blur-xl z-20 border-b'>
                 <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between '>
                     {/* Logo */}
                     <Link href="/" className=' flex items-center'>
@@ -19,6 +20,20 @@ const Header = () => {
 
                     {/* Search and location desktop view */}
                     {/* Right side actions */}
+                    <div className='flex items-center'>
+                        {/* Sign In / Sign Up buttons or User Profile */}
+                        <SignedOut>
+                            <SignInButton />
+                            <SignUpButton>
+                                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                    Sign Up
+                                </button>
+                            </SignUpButton>
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </div>
                 </div>
                 {/* Mobile search and location -below header */}
             </nav>
