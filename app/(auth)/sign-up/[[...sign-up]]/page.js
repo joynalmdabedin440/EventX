1c5a03ba032705165b25440c4c8e4be9d9447ec0
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-12-14 12:27:23 +0600

    add layout for sign in and signup page

--- app/(auth)/sign-up/[[...sign-up]]/page.js
+++ app/(auth)/sign-up/[[...sign-up]]/page.js
@@ -1,9 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
 import React from 'react'
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
+  return <SignUp/>
+    
+  
 }
 
 export default SignUpPage
