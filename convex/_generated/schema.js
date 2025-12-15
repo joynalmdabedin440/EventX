8ae97158f18f46e6d0bb77d98cdabf0a991f286a
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-12-15 23:50:26 +0600

    fix error on convex schema

--- convex/_generated/schema.js
+++
@@ -1,9 +0,0 @@
-import { defineSchema } from "convex/server";
-
-
-export default defineSchema({
-    users: defineTable({
-         name: v.string(),
-         tokenIdentifier: v.string(),
-  })
-})
---
+++ convex/schema.js
@@ -0,0 +1,30 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+
+export default defineSchema({
+    users: defineTable({
+        name: v.string(),
+        tokenIdentifier: v.string(),
+        email: v.string(),
+        imageUrl: v.optional(v.string()),
+
+        //onboarding fields
+        hasCompletedOnboarding: v.boolean(),
+        location: v.optional(
+            v.object({
+                city: v.string(),
+                state: v.optional(v.string()),
+                country: v.string(),
+            })
+        ),
+        interests: v.optional(v.array(v.string())),
+        //organizer tracking
+        freeEventsCreated: v.number(),
+
+        //timestamps
+        createdAt: v.number(),
+        updatedAt: v.number(),
+    }).index("by_token",["tokenIdentifier"]),
+
+})
