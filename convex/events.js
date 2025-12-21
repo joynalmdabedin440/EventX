0cccc29d9e36b0b2241f1a08ea564fce314eee6b
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-12-21 21:54:20 +0600

    add function of get events by location

--- convex/events.js
+++ convex/events.js
@@ -23,4 +23,41 @@ export const getFeaturedEvents = query({
     return featured;
 
   }
+});
+
+// get events by location (city and state)
+
+export const getEventsByLocation = query({
+  args: {
+    city: v.optional(v.string()),
+    state: v.optional(v.string()),
+    limit:v.optional(v.number())
+  },
+  handler: async (ctx, args) => {
+    const now = Date.now();
+
+    const events = await ctx.db
+      .query("events")
+      .withIndex("by_start_date")
+      .filter((q) => q.gte(q.field("startDate"), now))
+      .collect();
+    
+    //filter by city and state
+
+    if (args.city) {
+      events = events.filter(
+        (event)=> event.city.toLowerCase()===args.city.toLowerCase()
+      )
+    }
+    else if (args.state) {
+      events = events.filter(
+        (event)=> event.state?.toLowerCase()===args.state.toLowerCase()
+      )
+    }
+
+    return events.slice(0, args.limit ?? 4);
+    
+  },
+  
+
 })
