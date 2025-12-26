c0d97721f61c2384ccc42ecc65a9ed99463b25a1
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-12-27 01:51:09 +0600

    add explore page herro section

--- app/(public)/explore/page.js
+++ app/(public)/explore/page.js
@@ -7,27 +7,34 @@ import React, { use } from 'react'
 const ExplorePage = () => {
     //fetch current users
     const { data: currentUser } = useConvexQuery(api.users.getCurrentUser);
-    
-    
+
+
     //fetch featured events
     const { data: featuredEvents, isLoading: loadingFeatured } = useConvexQuery(api.events.getFeaturedEvents, { limit: 3 });
-    
+
     //fetch event by location
 
     const { data: localEvents, isLoading: loadingLocalEvents } = useConvexQuery(api.events.getEventsByLocation, { city: currentUser?.location?.city || 'Dhaka', state: currentUser?.location?.state || "Dhaka", limit: 4 });
 
     //get popular events
     const { data: popularEvents, isLoading: loadingPopular } = useConvexQuery(api.events.getPopularEvents, { limit: 6 });
-    
+
     //category count
     const { data: categoryCounts } = useConvexQuery(api.events.getCategoryCounts);
-    
-  return (
-    <div>
-      <h1 className="text-4xl font-bold mb-8">Explore Events</h1>
-      <p className="text-gray-400">Browse and discover amazing events</p>
-    </div>
-  )
+
+    return (
+        <>
+            {/* Hero Title */}
+            <div className="pb-12 text-center">
+                <h1 className="text-5xl md:text-6xl font-bold mb-4">Discover Events</h1>
+                <p className="text-lg text-muted-foreground max-w-3xl mx-auto">
+                    Explore featured events, find what&apos;s happening locally, or browse
+                    events across Bangladesh
+                </p>
+            </div>
+
+        </>
+    )
 }
 
 export default ExplorePage
