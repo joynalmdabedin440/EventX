450a39b9fe6a1ccb2b339abc7d335c1f5c4893a8
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-02-05 23:25:42 +0600

    add fetch event details and check registration user or not

--- app/(public)/events/[slug]/page.jsx
+++ app/(public)/events/[slug]/page.jsx
@@ -1,4 +1,6 @@
 "use client"
+import { api } from '@/convex/_generated/api';
+import { useConvexQuery } from '@/hooks/use-convex-query';
 import { useUser } from '@clerk/nextjs';
 import { useParams, useRouter } from 'next/navigation';
 import React, { useState } from 'react'
@@ -9,6 +11,18 @@ const EventPage = () => {
     const router = useRouter();
     const { user } = useUser();
     const [showRegisterModal, setShowRegisterModal] = useState(false);
+
+    // Fetch event details
+    const { data: event, isLoading } = useConvexQuery(api.events.getEventBySlug, {
+        slug: params.slug,
+    });
+
+    // Check if user is already registered
+    const { data: registration } = useConvexQuery(
+        api.registrations.checkRegistration,
+        event?._id ? { eventId: event._id } : "skip"
+    );
+    
     return (
         <div>EventPage</div>
     )
