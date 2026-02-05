46e5aed8b0bc15615eaea6b82af3b3afd7d15f09
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-02-06 00:42:03 +0600

    add title card of event registration page

--- app/(public)/events/[slug]/page.jsx
+++ app/(public)/events/[slug]/page.jsx
@@ -1,10 +1,14 @@
 "use client"
+import { Badge } from '@/components/ui/badge';
+import { format } from 'date-fns';
 import { api } from '@/convex/_generated/api';
 import { useConvexQuery } from '@/hooks/use-convex-query';
+import { getCategoryIcon, getCategoryLabel } from '@/lib/data';
 import { useUser } from '@clerk/nextjs';
-import { Loader2 } from 'lucide-react';
+import { Calendar, Clock, Loader2 } from 'lucide-react';
 import { notFound, useParams, useRouter } from 'next/navigation';
 import React, { useState } from 'react'
+
 
 const EventPage = () => {
 
@@ -32,12 +36,46 @@ const EventPage = () => {
         );
     }
 
-    if (!event) { 
+    if (!event) {
         notFound();
     }
 
     return (
-        <div>EventPage</div>
+        <div
+            style={{
+                backgroundColor: event.themeColor || "#1e3a8a",
+            }}
+            className="min-h-screen py-8 -mt-6 md:-mt-16 lg:-mx-5"
+        >
+            {/* Ui */}
+            <div className="max-w-7xl mx-auto px-8" >
+                {/* title card */}
+                <div className="mb-8">
+                    <Badge variant="secondary" className="mb-3">
+                        {getCategoryIcon(event.category)} {getCategoryLabel(event.category)}
+                    </Badge>
+                    <h1 className="text-4xl md:text-5xl font-bold mb-4">{event.title}</h1>
+                    <div className="flex flex-wrap items-center gap-4 text-muted-foreground">
+                        <div className="flex items-center gap-2">
+                            <Calendar className="w-5 h-5" />
+                            <span>{format(event.startDate, "EEEE, MMMM dd, yyyy")}</span>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <Clock className="w-5 h-5" />
+                            <span>
+                                {format(event.startDate, "h:mm a")} -{" "}
+                                {format(event.endDate, "h:mm a")}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            {/* register modal */}
+
+
+        </div>
     )
 }
 
